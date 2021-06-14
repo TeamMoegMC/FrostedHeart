50fd0bbbc413b1ecd222cc8cca0b111a90b26ed9
yuesha-yc <kevinwangyichen@gmail.com>
2021-06-14 17:12:12 +0800

    Add survive and unionlib as libraries

--- src/main/java/com/teammoeg/frostedheart/Generator.java
+++ src/main/java/com/teammoeg/frostedheart/Generator.java
@@ -1,4 +1,11 @@
 package com.teammoeg.frostedheart;
 
-public class Generator {
+import blusunrize.immersiveengineering.common.blocks.IEBlockInterfaces;
+import blusunrize.immersiveengineering.common.blocks.generic.MultiblockPartTileEntity;
+import blusunrize.immersiveengineering.common.blocks.stone.CokeOvenTileEntity;
+import blusunrize.immersiveengineering.common.util.inventory.IIEInventory;
+
+public class Generator  {
+    //extends MultiblockPartTileEntity<CokeOvenTileEntity> implements IIEInventory,
+    //        IEBlockInterfaces.IActiveState, IEBlockInterfaces.IInteractionObjectIE, IEBlockInterfaces.IProcessTile, IEBlockInterfaces.IBlockBounds
 }
---
+++ src/main/java/com/teammoeg/frostedheart/GeneratorMultiblock.java
@@ -0,0 +1,39 @@
+package com.teammoeg.frostedheart;
+
+import blusunrize.immersiveengineering.ImmersiveEngineering;
+import blusunrize.immersiveengineering.common.blocks.IEBlocks;
+import blusunrize.immersiveengineering.common.blocks.multiblocks.StoneMultiblock;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public class GeneratorMultiblock extends StoneMultiblock {
+    public GeneratorMultiblock()
+    {
+        super(new ResourceLocation("frostedheart", "multiblocks/generator"),
+                new BlockPos(1, 1, 1), new BlockPos(1, 1, 2), new BlockPos(3, 3, 3),
+                () -> IEBlocks.Multiblocks.cokeOven.getDefaultState());
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public boolean canRenderFormedStructure()
+    {
+        return false;
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void renderFormedStructure(MatrixStack transform, IRenderTypeBuffer buffer)
+    {
+    }
+
+    @Override
+    public float getManualScale()
+    {
+        return 16;
+    }
+}
