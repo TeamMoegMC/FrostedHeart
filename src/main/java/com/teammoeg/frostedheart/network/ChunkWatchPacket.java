7a4dc7f7543481bc2974dd5696f95d201d71e125
yuesha-yc <kevinwangyichen@gmail.com>
2021-08-09 00:52:01 +0800

    Update license & add cf and licener plugins & bump version

--- src/main/java/com/teammoeg/frostedheart/network/ChunkWatchPacket.java
+++ src/main/java/com/teammoeg/frostedheart/network/ChunkWatchPacket.java
@@ -1,6 +1,19 @@
 /*
- * Work under Copyright. Licensed under the EUPL.
- * See the project README.md and LICENSE.txt for more information.
+ * Copyright (c) 2021 TeamMoeg
+ *
+ * This file is part of Frosted Heart.
+ *
+ * Frosted Heart is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Frosted Heart is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Frosted Heart. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package com.teammoeg.frostedheart.network;
