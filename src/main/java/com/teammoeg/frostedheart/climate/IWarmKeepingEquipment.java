d551cd2a627797c94f27a81039c2ca81b18e8b7d
yuesha-yc <kevinwangyichen@gmail.com>
2021-09-25 19:08:54 -0700

    update licenses

--- src/main/java/com/teammoeg/frostedheart/climate/IWarmKeepingEquipment.java
+++ src/main/java/com/teammoeg/frostedheart/climate/IWarmKeepingEquipment.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2021 TeamMoeg
+ *
+ * This file is part of Frosted Heart.
+ *
+ * Frosted Heart is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Frosted Heart is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Frosted Heart. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.teammoeg.frostedheart.climate;
 
 import net.minecraft.entity.player.ServerPlayerEntity;
--- src/main/java/com/teammoeg/frostedheart/climate/chunkdata/SphericTemperatureAdjust.java
+++ src/main/java/com/teammoeg/frostedheart/climate/chunkdata/SphericTemperatureAdjust.java
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Frosted Heart. If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.teammoeg.frostedheart.climate.chunkdata;
 
 import net.minecraft.nbt.CompoundNBT;
