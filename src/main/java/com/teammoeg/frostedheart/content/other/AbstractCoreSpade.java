a003537751aeee2949cadc8e4a5566d90fa7865c
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2021-10-31 22:09:30 +0800

    fixed error report

--- src/main/java/com/teammoeg/frostedheart/content/other/AbstractCoreSpade.java
+++ src/main/java/com/teammoeg/frostedheart/content/other/AbstractCoreSpade.java
@@ -18,13 +18,14 @@
 
 package com.teammoeg.frostedheart.content.other;
 
-
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
+import java.util.Set;
+import java.util.function.Predicate;
 
 import com.teammoeg.frostedheart.base.item.FHBaseItem;
 import com.teammoeg.frostedheart.client.util.GuiUtils;
@@ -47,73 +48,88 @@ import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.util.FakePlayer;
 
 public abstract class AbstractCoreSpade extends FHBaseItem {
-    public static ResourceLocation otag = new ResourceLocation("forge:ores");
-    public static ResourceLocation stag = new ResourceLocation("forge:stone");
-    public AbstractCoreSpade(String name, Properties properties) {
-        super(name, properties);
-    }
-    public abstract int getHorizonalRange(ItemStack item);
-    public abstract int getVerticalRange(ItemStack item);
-    @SuppressWarnings("resource")
+	public static ResourceLocation otag = new ResourceLocation("forge:ores");
+	public static ResourceLocation stag = new ResourceLocation("forge:stone");
+
+	public AbstractCoreSpade(String name, Properties properties) {
+		super(name, properties);
+	}
+
+	public abstract int getHorizonalRange(ItemStack item);
+
+	public abstract int getVerticalRange(ItemStack item);
+
+	@SuppressWarnings("resource")
 	@Override
-    public ActionResultType onItemUse(ItemUseContext context) {
-        PlayerEntity player = context.getPlayer();
-        if (player != null&&(!(player instanceof FakePlayer))) {//fake players does not deserve XD
-	        World world = context.getWorld();
-	        BlockPos blockpos = context.getPos();
-	        if(world.getBlockState(blockpos).getBlock().getTags().contains(otag)) {//early exit 'cause ore found
-	        	player.sendStatusMessage(new TranslationTextComponent(world.getBlockState(blockpos).getBlock().getTranslationKey()).mergeStyle(TextFormatting.GOLD), true);
-	        	 return ActionResultType.SUCCESS;
-	        }
-	        int x = blockpos.getX();
-	        int y = blockpos.getY();
-	        int z = blockpos.getZ();
-	        context.getItem().damageItem(1, player, (player2) -> player2.sendBreakAnimation(context.getHand()));
-	        if(!world.isRemote) {
-		        Random rnd=new Random(BlockPos.pack(x, y, z)^0x9a6dc5270b92313dL);//randomize
-		        //This is predictable, but not any big problem. Cheaters can use x-ray or other things rather then hacking in this. 
-		        if(rnd.nextInt(20)!=0) {//mistaken rate 5%
-			        BlockPos.Mutable mutable = new BlockPos.Mutable(x, y, z);
-			        Block ore;
-			        HashMap<String,Integer> founded=new HashMap<>();
-			        int hrange=this.getHorizonalRange(context.getItem());
-			        int vrange=this.getVerticalRange(context.getItem());
-			        vrange=Math.min(y,(rnd.nextInt(vrange)+vrange)/2);
-			        
-			        for (int x2 = -hrange; x2 < hrange; x2++)
-			            for (int y2 = -vrange; y2 < 0; y2++)
-			                for (int z2 = -hrange; z2 < hrange; z2++) {
-			                    int BlockX = x + x2;
-			                    int BlockY = y + y2;
-			                    int BlockZ = z + z2;
-			                    ore = world.getBlockState(mutable.setPos(BlockX, BlockY, BlockZ)).getBlock();
-			                    if (ore.getTags().contains(otag)||ore.getTags().contains(stag)) {
-			                        founded.merge(ore.getTranslationKey(),1,(a,b)->a+b);
-			                    }
-			                }
-		        
-		            if (!founded.isEmpty()) {
-		            	int count=0;
-		            	IFormattableTextComponent s=GuiUtils.translateMessage("corespade.ore");
-		            	for(Entry<String, Integer> f:founded.entrySet()) {
-		            		if(rnd.nextInt(f.getValue())!=0) {
-		            			s=s.appendSibling(new TranslationTextComponent(f.getKey()).mergeStyle(TextFormatting.GREEN).appendString(" "));
-		            			count++;
-		            		}
-		            	}
-		            	if(count>0) {
-		            		player.sendMessage(s,player.getUniqueID());
-		            		return ActionResultType.SUCCESS;
-		            	}
-		            }
-		        }
-	            player.sendMessage(GuiUtils.translateMessage("corespade.nothing").mergeStyle(TextFormatting.GRAY),player.getUniqueID());
-	        }
-        }
-        return ActionResultType.SUCCESS;
-    }
-    @Override
-    public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
-        tooltip.add(GuiUtils.translateTooltip("meme.core_spade").mergeStyle(TextFormatting.GRAY));
-    }
+	public ActionResultType onItemUse(ItemUseContext context) {
+		PlayerEntity player = context.getPlayer();
+		if (player != null && (!(player instanceof FakePlayer))) {// fake players does not deserve XD
+			World world = context.getWorld();
+			BlockPos blockpos = context.getPos();
+			if (world.getBlockState(blockpos).getBlock().getTags().contains(otag)) {// early exit 'cause ore found
+				player.sendStatusMessage(
+						new TranslationTextComponent(world.getBlockState(blockpos).getBlock().getTranslationKey())
+								.mergeStyle(TextFormatting.GOLD),
+						true);
+				return ActionResultType.SUCCESS;
+			}
+			int x = blockpos.getX();
+			int y = blockpos.getY();
+			int z = blockpos.getZ();
+			context.getItem().damageItem(1, player, (player2) -> player2.sendBreakAnimation(context.getHand()));
+			if (!world.isRemote) {
+				Random rnd = new Random(BlockPos.pack(x, y, z) ^ 0x9a6dc5270b92313dL);// randomize
+				// This is predictable, but not any big problem. Cheaters can use x-ray or other
+				// things rather then hacking in this.
+
+				Predicate<Set<ResourceLocation>> tagdet;
+				if (rnd.nextInt(20) != 0) {
+					tagdet = ts -> (ts.contains(otag)) || ts.contains(stag);
+				} else
+					tagdet = ts -> ts.contains(stag);
+				BlockPos.Mutable mutable = new BlockPos.Mutable(x, y, z);
+				Block ore;
+				HashMap<String, Integer> founded = new HashMap<>();
+				int hrange = this.getHorizonalRange(context.getItem());
+				int vrange = this.getVerticalRange(context.getItem());
+				vrange = Math.min(y, (rnd.nextInt(vrange) + vrange) / 2);
+
+				for (int x2 = -hrange; x2 < hrange; x2++)
+					for (int y2 = -vrange; y2 < 0; y2++)
+						for (int z2 = -hrange; z2 < hrange; z2++) {
+							int BlockX = x + x2;
+							int BlockY = y + y2;
+							int BlockZ = z + z2;
+							ore = world.getBlockState(mutable.setPos(BlockX, BlockY, BlockZ)).getBlock();
+							if (tagdet.test(ore.getTags())) {
+								founded.merge(ore.getTranslationKey(), 1, (a, b) -> a + b);
+							}
+						}
+
+				if (!founded.isEmpty()) {
+					int count = 0;
+					IFormattableTextComponent s = GuiUtils.translateMessage("corespade.ore");
+					for (Entry<String, Integer> f : founded.entrySet()) {
+						if (rnd.nextInt(f.getValue()) != 0) {
+							s = s.appendSibling(new TranslationTextComponent(f.getKey())
+									.mergeStyle(TextFormatting.GREEN).appendString(" "));
+							count++;
+						}
+					}
+					if (count > 0) {
+						player.sendMessage(s, player.getUniqueID());
+						return ActionResultType.SUCCESS;
+					}
+				}
+				player.sendMessage(GuiUtils.translateMessage("corespade.nothing").mergeStyle(TextFormatting.GRAY),
+						player.getUniqueID());
+			}
+		}
+		return ActionResultType.SUCCESS;
+	}
+
+	@Override
+	public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+		tooltip.add(GuiUtils.translateTooltip("meme.core_spade").mergeStyle(TextFormatting.GRAY));
+	}
 }
