8219a3805d145a5cb69b8bf1225b9037daba5430
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2021-11-06 23:29:11 +0800

    add starup item name

--- src/main/java/com/teammoeg/frostedheart/events/ForgeEvents.java
+++ src/main/java/com/teammoeg/frostedheart/events/ForgeEvents.java
@@ -259,10 +259,10 @@ public class ForgeEvents {
         if (!persistent.contains(FHNBT.FIRST_LOGIN_GIVE_MANUAL)) {
             persistent.putBoolean(FHNBT.FIRST_LOGIN_GIVE_MANUAL, false);
             event.getPlayer().inventory.addItemStackToInventory(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation("ftbquests", "book"))));
-            event.getPlayer().inventory.armorInventory.set(3, FHNBT.ArmorNBT(new ItemStack(Items.IRON_HELMET)));
-            event.getPlayer().inventory.armorInventory.set(2, FHNBT.ArmorNBT(new ItemStack(Items.IRON_CHESTPLATE)));
-            event.getPlayer().inventory.armorInventory.set(1, FHNBT.ArmorNBT(new ItemStack(Items.IRON_LEGGINGS)));
-            event.getPlayer().inventory.armorInventory.set(0, FHNBT.ArmorNBT(new ItemStack(Items.IRON_BOOTS)));
+            event.getPlayer().inventory.armorInventory.set(3, FHNBT.ArmorNBT(new ItemStack(Items.IRON_HELMET).setDisplayName(new TranslationTextComponent(  "itemname.frostedheart.start_head"))));
+            event.getPlayer().inventory.armorInventory.set(2, FHNBT.ArmorNBT(new ItemStack(Items.IRON_CHESTPLATE).setDisplayName(new TranslationTextComponent("itemname.frostedheart.start_chest"))));
+            event.getPlayer().inventory.armorInventory.set(1, FHNBT.ArmorNBT(new ItemStack(Items.IRON_LEGGINGS).setDisplayName(new TranslationTextComponent("itemname.frostedheart.start_leg"))));
+            event.getPlayer().inventory.armorInventory.set(0, FHNBT.ArmorNBT(new ItemStack(Items.IRON_BOOTS).setDisplayName(new TranslationTextComponent("itemname.frostedheart.start_foot"))));
             ItemStack breads = new ItemStack(Items.BREAD);
             breads.setCount(16);
             event.getPlayer().inventory.addItemStackToInventory(breads);
