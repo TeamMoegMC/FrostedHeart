a60adcec1792e6ee81a4ffcab5465fec0f2f6646
yuesha-yc <kevinwangyichen@gmail.com>
2021-11-06 23:49:15 -0700

    research basic classes

---
+++ src/main/java/com/teammoeg/frostedheart/research/Research.java
@@ -0,0 +1,112 @@
+package com.teammoeg.frostedheart.research;
+
+import com.teammoeg.frostedheart.FHMain;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TranslationTextComponent;
+
+import java.util.HashSet;
+
+/**
+ * The base definition of Research.
+ * Part of Research Line {@link ResearchLine}
+ *
+ */
+public class Research {
+    private ResourceLocation id;
+    private TranslationTextComponent name;
+    private TranslationTextComponent desc;
+    private Item icon;
+    private HashSet<Research> parents = new HashSet<>();
+    private ResearchLine line;
+    private boolean completed;
+
+    public Research(String path, Research... parents) {
+        this(new ResourceLocation(FHMain.MODID, path), Items.GRASS_BLOCK, parents);
+    }
+
+    public Research(String path, Item icon, Research... parents) {
+        this(new ResourceLocation(FHMain.MODID, path), icon, parents);
+    }
+
+    public Research(ResourceLocation id, Item icon, Research... parents) {
+        this.id = id;
+        for (Research parent : parents) this.parents.add(parent);
+        this.name = new TranslationTextComponent("research."+id.getNamespace() + "." + id.getPath() + ".name");
+        this.desc = new TranslationTextComponent("research."+id.getNamespace() + "." + id.getPath() + ".desc");
+        this.icon = icon;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void complete() {
+        this.completed = true;
+    }
+
+    public boolean isUnlocked() {
+        if (!this.parents.isEmpty()) {
+            for (Research research : parents) {
+                if (!research.isCompleted()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isHidden() {
+        if (!this.parents.isEmpty()) {
+            for (Research research : parents) {
+                if (!research.isUnlocked()) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public ResourceLocation getId() {
+        return id;
+    }
+
+    public void setId(ResourceLocation id) {
+        this.id = id;
+    }
+
+    public HashSet<Research> getParents() {
+        return parents;
+    }
+
+    public void setParents(Research... parents) {
+        HashSet<Research> newSet = new HashSet<>();
+        for (Research parent : parents) newSet.add(parent);
+        this.parents = newSet;
+    }
+    
+    public Item getIcon() {
+        return icon;
+    }
+
+    public TranslationTextComponent getName() {
+        return name;
+    }
+
+    public TranslationTextComponent getDesc() {
+        return desc;
+    }
+
+    public ResearchLine getLine() {
+        return line;
+    }
+
+    public void setLine(ResearchLine line) {
+        this.line = line;
+    }
+
+    public String toString() {
+        return "Research[" + id + "]";
+    }
+}
---
+++ src/main/java/com/teammoeg/frostedheart/research/ResearchLine.java
@@ -0,0 +1,58 @@
+/*
+ *  Copyright (c) 2021. TeamMoeg
+ *
+ *  This file is part of Energy Level Transition.
+ *
+ *  Energy Level Transition is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  Energy Level Transition is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Energy Level Transition.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.teammoeg.frostedheart.research;
+
+import com.teammoeg.frostedheart.FHMain;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TranslationTextComponent;
+
+public class ResearchLine {
+    private ResourceLocation id;
+    private TranslationTextComponent name;
+    private TranslationTextComponent desc;
+    private Item icon;
+
+    public ResearchLine(String path, Item icon) {
+        this(new ResourceLocation(FHMain.MODID, path), icon);
+    }
+
+    public ResearchLine(ResourceLocation id, Item icon) {
+        this.id = id;
+        this.name = new TranslationTextComponent("research_line."+id.getNamespace() + "." + id.getPath() + ".name");
+        this.desc = new TranslationTextComponent("research_line."+id.getNamespace() + "." + id.getPath() + ".desc");
+        this.icon = icon;
+    }
+
+    public Item getIcon() {
+        return icon;
+    }
+
+    public TranslationTextComponent getDesc() {
+        return desc;
+    }
+
+    public TranslationTextComponent getName() {
+        return name;
+    }
+
+    public ResourceLocation getId() {
+        return id;
+    }
+}
