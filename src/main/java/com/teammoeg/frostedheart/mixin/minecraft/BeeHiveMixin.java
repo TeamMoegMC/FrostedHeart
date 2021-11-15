380d92939697a142e741ecfd0d0c466f1c14a255
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2021-11-15 18:47:59 +0800

    bees no longer work in cold environment

---
+++ src/main/java/com/teammoeg/frostedheart/mixin/minecraft/BeeHiveMixin.java
@@ -0,0 +1,24 @@
+package com.teammoeg.frostedheart.mixin.minecraft;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.teammoeg.frostedheart.climate.chunkdata.ChunkData;
+
+import net.minecraft.tileentity.BeehiveTileEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityType;
+@Mixin(BeehiveTileEntity.class)
+public class BeeHiveMixin extends TileEntity {
+
+	public BeeHiveMixin(TileEntityType<?> tileEntityTypeIn) {
+		super(tileEntityTypeIn);
+	}
+	@Inject(at=@At("HEAD"),method="tick",cancellable=true)
+	public void tick(CallbackInfo cbi) {
+		if(!world.isRemote&&ChunkData.getTemperature(world, pos)<14)
+			cbi.cancel();
+	} 
+}
