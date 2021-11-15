085722099c6f1fdd2c30d9ecceb053b5c14a8594
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2021-11-15 17:03:22 +0800

    no longer sleep over weather

---
+++ src/main/java/com/teammoeg/frostedheart/mixin/minecraft/MixinServerWorld.java
@@ -0,0 +1,31 @@
+package com.teammoeg.frostedheart.mixin.minecraft;
+
+import java.util.function.Supplier;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+
+import net.minecraft.profiler.IProfiler;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.world.DimensionType;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.storage.ISpawnWorldInfo;
+@Mixin(ServerWorld.class)
+public abstract class MixinServerWorld extends World {
+
+	protected MixinServerWorld(ISpawnWorldInfo worldInfo, RegistryKey<World> dimension, DimensionType dimensionType,
+			Supplier<IProfiler> profiler, boolean isRemote, boolean isDebug, long seed) {
+		super(worldInfo, dimension, dimensionType, profiler, isRemote, isDebug, seed);
+	}
+    /**
+     * @author khjxiaogu
+     * @reason Not allow sleep over weather
+     */
+	@Overwrite
+	private void resetRainAndThunder() {
+		
+	}
+
+}
