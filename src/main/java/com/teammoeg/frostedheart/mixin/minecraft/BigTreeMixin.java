28a278c40cf998226d44ddf7584d33fc947e9299
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2021-11-16 17:11:44 +0800

    Big trees grow a lot slow

---
+++ src/main/java/com/teammoeg/frostedheart/mixin/minecraft/BigTreeMixin.java
@@ -0,0 +1,23 @@
+package com.teammoeg.frostedheart.mixin.minecraft;
+
+import java.util.Random;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.trees.BigTree;
+import net.minecraft.block.trees.Tree;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.server.ServerWorld;
+@Mixin(BigTree.class)
+public abstract class BigTreeMixin extends Tree {
+	@Inject(at=@At("HEAD"),method="placeMega",cancellable=true)
+	public void btmplaceMega(ServerWorld p_235678_1_, ChunkGenerator p_235678_2_, BlockPos p_235678_3_, BlockState p_235678_4_, Random p_235678_5_, int p_235678_6_, int p_235678_7_,CallbackInfoReturnable<Boolean> cr) {
+		if(p_235678_5_.nextInt(8)!=0)
+			cr.setReturnValue(false);
+	}
+}
