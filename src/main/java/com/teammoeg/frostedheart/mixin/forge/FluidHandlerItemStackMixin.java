3dfbfbeded95177c8750d0b3662808b50ad68a81
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2021-11-16 13:53:59 +0800

    general liquid container fix

---
+++ src/main/java/com/teammoeg/frostedheart/mixin/forge/FluidHandlerItemStackMixin.java
@@ -0,0 +1,21 @@
+package com.teammoeg.frostedheart.mixin.forge;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.IFluidHandlerItem;
+import net.minecraftforge.fluids.capability.templates.FluidHandlerItemStack;
+@Mixin(FluidHandlerItemStack.class)
+public abstract class FluidHandlerItemStackMixin implements IFluidHandlerItem, ICapabilityProvider {
+    /**
+     * @author khjxiaogu
+     * @reason Forge set default return true, causing bugs. So set default check
+     */
+	@Overwrite(remap=false)
+    public boolean canFillFluidType(FluidStack fluid)
+    {
+        return isFluidValid(0,fluid);
+    }
+}
