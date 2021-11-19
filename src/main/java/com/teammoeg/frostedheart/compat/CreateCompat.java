4382ca5fc11c48a3564dfe7b4afd491b6726f6e7
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2021-11-19 10:16:19 +0800

    stress for funnels

--- src/main/java/com/teammoeg/frostedheart/compat/CreateCompat.java
+++ src/main/java/com/teammoeg/frostedheart/compat/CreateCompat.java
@@ -1,11 +1,18 @@
 package com.teammoeg.frostedheart.compat;
 
+import com.simibubi.create.AllBlocks;
 import com.simibubi.create.foundation.block.BlockStressDefaults;
+
+import net.minecraft.block.Blocks;
 import net.minecraft.util.ResourceLocation;
 
 public class CreateCompat {
     public static void init() {
-        BlockStressDefaults.setDefaultImpact(new ResourceLocation("create","mechanical_harvester"),4.0);
-        BlockStressDefaults.setDefaultImpact(new ResourceLocation("create","mechanical_plough"),4.0);
+        BlockStressDefaults.setDefaultImpact(AllBlocks.MECHANICAL_HARVESTER.getId(),4.0);
+        BlockStressDefaults.setDefaultImpact(AllBlocks.MECHANICAL_PLOUGH.getId(),4.0);
+        BlockStressDefaults.setDefaultImpact(AllBlocks.ANDESITE_FUNNEL.getId(),4.0);
+        BlockStressDefaults.setDefaultImpact(AllBlocks.BRASS_FUNNEL.getId(),4.0);
+        BlockStressDefaults.setDefaultImpact(Blocks.DISPENSER.getRegistryName(),4.0);
+        
     }
 }
