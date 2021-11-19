be93e0c51c486c577b55a234a0d4710408bc7d57
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2021-11-19 18:34:09 +0800

    swamp hut table fix

---
+++ src/main/java/com/teammoeg/frostedheart/mixin/minecraft/MixinSwampHutPiece.java
@@ -0,0 +1,35 @@
+package com.teammoeg.frostedheart.mixin.minecraft;
+
+import java.util.Random;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.CraftingTableBlock;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.world.ISeedReader;
+import net.minecraft.world.gen.feature.structure.IStructurePieceType;
+import net.minecraft.world.gen.feature.structure.ScatteredStructurePiece;
+import net.minecraft.world.gen.feature.structure.SwampHutPiece;
+@Mixin(SwampHutPiece.class)
+public abstract class MixinSwampHutPiece extends ScatteredStructurePiece {
+
+	public MixinSwampHutPiece(IStructurePieceType structurePieceTypeIn, Random rand, int xIn, int yIn, int zIn,
+			int widthIn, int heightIn, int depthIn) {
+		super(structurePieceTypeIn, rand, xIn, yIn, zIn, widthIn, heightIn, depthIn);
+	}
+
+	@Override
+	protected void setBlockState(ISeedReader worldIn, BlockState blockstateIn, int x, int y, int z,
+			MutableBoundingBox boundingboxIn) {
+		if(blockstateIn!=null&&blockstateIn.getBlock() instanceof CraftingTableBlock)return;
+		
+		super.setBlockState(worldIn, blockstateIn, x, y, z, boundingboxIn);
+	}
+
+	protected MixinSwampHutPiece(IStructurePieceType structurePieceTypeIn, CompoundNBT nbt) {
+		super(structurePieceTypeIn, nbt);
+	}
+
+}
