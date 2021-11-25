0fc81d13525c15741cdd631579e6469a458c18f6
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2021-11-25 15:57:41 +0800

    change to mixin temp manager

--- src/main/java/com/teammoeg/frostedheart/mixin/minecraft/SingleJigsawPieceMixin.java
+++ src/main/java/com/teammoeg/frostedheart/mixin/minecraft/SingleJigsawPieceMixin.java
@@ -1,37 +1,42 @@
 package com.teammoeg.frostedheart.mixin.minecraft;
 
-import java.util.function.Function;
+import java.util.Map;
+import javax.annotation.Nullable;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-import com.mojang.datafixers.util.Either;
 import com.teammoeg.frostedheart.util.StructureUtils;
 
-import net.minecraft.world.gen.feature.jigsaw.JigsawPattern.PlacementBehaviour;
 import net.minecraft.world.gen.feature.template.Template;
 import net.minecraft.world.gen.feature.template.TemplateManager;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.gen.feature.jigsaw.JigsawPiece;
-import net.minecraft.world.gen.feature.jigsaw.SingleJigsawPiece;
 
-@Mixin(SingleJigsawPiece.class)
-public abstract class SingleJigsawPieceMixin extends JigsawPiece {
-	protected SingleJigsawPieceMixin(PlacementBehaviour projection) {
-		super(projection);
-	}
+@Mixin(TemplateManager.class)
+public abstract class SingleJigsawPieceMixin {
 	@Shadow
-	protected Either<ResourceLocation, Template> field_236839_c_;
+	private Map<ResourceLocation, Template> templates;
+
 	/**
 	 * @author khjxiaogu
 	 * @reason auto remake structures
 	 */
 	@Overwrite
-	private Template func_236843_a_(TemplateManager p_236843_1_) {
-		Template t=this.field_236839_c_.map(p_236843_1_::getTemplateDefaulted, Function.identity());
-		//StructureUtils.handlePalette(t.);
-		StructureUtils.handlePalette(((TemplateAccess)t).getBlocks());
-		return t;
+	@Nullable
+	public Template getTemplate(ResourceLocation p_200219_1_) {
+		return this.templates.computeIfAbsent(p_200219_1_, (p_209204_1_) -> {
+			Template template = this.loadTemplateFile(p_209204_1_);
+			StructureUtils.handlePalette(((TemplateAccess) template).getBlocks());
+			return template != null ? template : this.loadTemplateResource(p_209204_1_);
+		});
 	}
+
+	@Shadow
+	@Nullable
+	abstract Template loadTemplateResource(ResourceLocation p_209201_1_);
+
+	@Shadow
+	@Nullable
+	abstract Template loadTemplateFile(ResourceLocation locationIn);
 }
