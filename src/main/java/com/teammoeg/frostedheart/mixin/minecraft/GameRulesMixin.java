8247e9f6d162d0a72e78df1fe8a6754bfaa613ba
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2021-12-10 15:23:15 +0800

    merge

--- src/main/java/com/teammoeg/frostedheart/mixin/minecraft/GameRulesMixin.java
+++ src/main/java/com/teammoeg/frostedheart/mixin/minecraft/GameRulesMixin.java
@@ -10,8 +10,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class GameRulesMixin {
     @Inject(method = "getBoolean", at = @At(value = "HEAD"),cancellable=true)
     public void disableWeatherCycle(GameRules.RuleKey<GameRules.BooleanValue> key, CallbackInfoReturnable<Boolean> cir) {
-        /*if (key == GameRules.DO_WEATHER_CYCLE) {
+        if (key == GameRules.DO_WEATHER_CYCLE) {
             cir.setReturnValue(false);
-        }*/
+        }
     }
 }
