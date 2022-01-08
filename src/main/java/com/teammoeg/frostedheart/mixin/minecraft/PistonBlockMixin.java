faf7241a4a02ad8ec2c1f209e33623c1c75dec14
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2022-01-08 16:13:17 +0800

    fix piston glitch?

---
+++ src/main/java/com/teammoeg/frostedheart/mixin/minecraft/PistonBlockMixin.java
@@ -0,0 +1,19 @@
+package com.teammoeg.frostedheart.mixin.minecraft;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+import net.minecraft.block.DirectionalBlock;
+import net.minecraft.block.PistonBlock;
+
+@Mixin(PistonBlock.class)
+public abstract class PistonBlockMixin extends DirectionalBlock {
+	protected PistonBlockMixin(Properties builder) {
+		super(builder);
+	}
+	@ModifyConstant(method="doMove",constant=@Constant(intValue=68,ordinal=0))
+	public int getFlag(int in) {
+		return in|16;
+	}
+}
