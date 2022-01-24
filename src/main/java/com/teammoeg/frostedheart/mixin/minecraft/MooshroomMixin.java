640869811be23c7cd78c13f7e66e7cd055fb6e37
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2022-01-24 21:24:48 +0800

    mixin moo

---
+++ src/main/java/com/teammoeg/frostedheart/mixin/minecraft/MooshroomMixin.java
@@ -0,0 +1,46 @@
+package com.teammoeg.frostedheart.mixin.minecraft;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.teammoeg.frostedheart.client.util.GuiUtils;
+import com.teammoeg.frostedheart.util.IMilkable;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.CowEntity;
+import net.minecraft.entity.passive.MooshroomEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+@Mixin(MooshroomEntity.class)
+public class MooshroomMixin extends CowEntity {
+
+	public MooshroomMixin(EntityType<? extends CowEntity> p_i48567_1_, World p_i48567_2_) {
+		super(p_i48567_1_, p_i48567_2_);
+	}
+
+	@Inject(at=@At("HEAD"),method="getEntityInteractionResult",cancellable=true)
+	public void fhmo$getEntityInteractionResult(PlayerEntity playerIn, Hand hand,CallbackInfoReturnable<ActionResultType> cbi) {
+		ItemStack itemstack = playerIn.getHeldItem(hand);
+		if (itemstack.getItem() == Items.BOWL && !this.isChild()) {
+			CowEntityMixin ot=(CowEntityMixin)(Object) this;
+			
+			if(ot.milk<=0) {
+				if (!world.isRemote) {
+					if (ot.feeded <= 0)
+						playerIn.sendMessage(GuiUtils.translateMessage("cow.nomilk.hungry"), playerIn.getUniqueID());
+					else
+						playerIn.sendMessage(GuiUtils.translateMessage("cow.nomilk.digest"), playerIn.getUniqueID());
+				}
+				cbi.setReturnValue(ActionResultType.PASS);
+			}
+			ot.milk--;
+		}
+	}
+
+}
