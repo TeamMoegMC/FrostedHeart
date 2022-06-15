64bdca2af4633119b40c4676ec60f610646f9f01
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2022-06-16 03:06:17 +0800

    add clock source

---
+++ src/main/java/com/teammoeg/frostedheart/climate/WorldClockSource.java
@@ -0,0 +1,47 @@
+package com.teammoeg.frostedheart.climate;
+
+import net.minecraft.nbt.CompoundNBT;
+
+public class WorldClockSource {
+	long secs;
+	long lastdaytime;
+	public WorldClockSource() {
+	}
+	public void update(long newTime) {
+		long cdt=newTime;
+		long dt=cdt-lastdaytime;
+		if(dt<0) {
+			long nextday=lastdaytime+24000L;
+			nextday=nextday-nextday%24000L;//assumpt it's next day and continue
+			dt=cdt+nextday-lastdaytime;
+		}
+		
+		long ndt=cdt-dt%20;
+		secs+=dt/20;
+		lastdaytime=ndt;
+	}
+	public int getHourInDay() {
+		return (int) ((secs/50)%24);
+	}
+	public long getDate() {
+		return (secs/50)/24;
+	}
+	public long getMonth() {
+		return (secs/50)/24/30;
+	}
+	public long getTimeSecs() {
+		return secs;
+	}
+	public CompoundNBT serialize(CompoundNBT cnbt) {
+		cnbt.putLong("secs",secs);
+		cnbt.putLong("last", lastdaytime);
+		return cnbt;
+	}
+	public CompoundNBT serialize() {
+		return serialize(new CompoundNBT());
+	}
+	public void read(CompoundNBT cnbt) {
+		secs=cnbt.getLong("secs");
+		lastdaytime=cnbt.getLong("last");
+	}
+}
