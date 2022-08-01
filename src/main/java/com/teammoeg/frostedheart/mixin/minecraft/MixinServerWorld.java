03e2370b931720c20fed2b92a98fa13808f42bbd
yuesha-yc <kevinwangyichen@gmail.com>
2022-07-31 23:46:08 -0700

    Use our temperature system to control world weather

--- src/main/java/com/teammoeg/frostedheart/mixin/minecraft/MixinServerWorld.java
+++ src/main/java/com/teammoeg/frostedheart/mixin/minecraft/MixinServerWorld.java
@@ -1,8 +1,14 @@
 package com.teammoeg.frostedheart.mixin.minecraft;
 
-import java.util.function.BooleanSupplier;
-import java.util.function.Supplier;
-
+import com.teammoeg.frostedheart.climate.ClimateData;
+import com.teammoeg.frostedheart.util.FHGameRule;
+import net.minecraft.profiler.IProfiler;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.world.DimensionType;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.storage.IServerWorldInfo;
+import net.minecraft.world.storage.ISpawnWorldInfo;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -11,129 +17,84 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.teammoeg.frostedheart.climate.ClimateData;
-import com.teammoeg.frostedheart.network.PacketHandler;
-import com.teammoeg.frostedheart.network.climate.FHClimatePacket;
-import com.teammoeg.frostedheart.util.FHGameRule;
-
-import net.minecraft.profiler.IProfiler;
-import net.minecraft.util.RegistryKey;
-import net.minecraft.world.DimensionType;
-import net.minecraft.world.World;
-import net.minecraft.world.server.ServerWorld;
-import net.minecraft.world.storage.IServerWorldInfo;
-import net.minecraft.world.storage.ISpawnWorldInfo;
-import net.minecraftforge.fml.network.PacketDistributor;
+import java.util.function.BooleanSupplier;
+import java.util.function.Supplier;
 
 @Mixin(ServerWorld.class)
 public abstract class MixinServerWorld extends World {
 
-	@Shadow @Final public IServerWorldInfo serverWorldInfo;
+    @Shadow
+    @Final
+    public IServerWorldInfo serverWorldInfo;
 
-	protected MixinServerWorld(ISpawnWorldInfo worldInfo, RegistryKey<World> dimension, DimensionType dimensionType,
-							   Supplier<IProfiler> profiler, boolean isRemote, boolean isDebug, long seed) {
-		super(worldInfo, dimension, dimensionType, profiler, isRemote, isDebug, seed);
-	}
+    protected MixinServerWorld(ISpawnWorldInfo worldInfo, RegistryKey<World> dimension, DimensionType dimensionType,
+                               Supplier<IProfiler> profiler, boolean isRemote, boolean isDebug, long seed) {
+        super(worldInfo, dimension, dimensionType, profiler, isRemote, isDebug, seed);
+    }
+
     /**
      * @author khjxiaogu
      * @reason Not allow sleep over weather
      */
-	@Overwrite
-	private void resetRainAndThunder() {
-		
-	}
+    @Overwrite
+    private void resetRainAndThunder() {
 
-	/**
-	 * @author yuesha-yc
-	 * @reason this allows us to add our own weather logic since we disabled it
-	 * @see GameRulesMixin#disableWeatherCycle
-	 */
-	@Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/DimensionType;hasSkyLight()Z"))
-	private void tick(BooleanSupplier hasTimeLeft, CallbackInfo ci) {
-		final ClimateData cap = this.getCapability(ClimateData.CAPABILITY,null).orElseThrow(() -> new IllegalStateException("Expected Climate Data to exist on World " + this.getDimensionKey() + " / " + this.getDimensionType()));
-		if(!((FHGameRule)this.getGameRules()).isWeatherCycle())//vanilla rules
-			return;
-		// vanilla weather params
-		int clearTime = this.serverWorldInfo.getClearWeatherTime();
-		int thunderTime = this.serverWorldInfo.getThunderTime();
-		int rainTime = this.serverWorldInfo.getRainTime();
-		boolean isThundering = this.worldInfo.isThundering();
-		boolean isRaining = this.worldInfo.isRaining();
+    }
 
-		// twr weather params
-		int blizzardTime = cap.getBlizzardTime();
-		boolean isBlizzard = cap.isBlizzard();
+    /**
+     * @author yuesha-yc
+     * @reason this allows us to add our own weather logic since we disabled it
+     * @see GameRulesMixin#disableWeatherCycle
+     */
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/DimensionType;hasSkyLight()Z"))
+    private void tick(BooleanSupplier hasTimeLeft, CallbackInfo ci) {
+        if (!((FHGameRule) this.getGameRules()).isWeatherCycle())//vanilla rules
+            return;
 
-		// if currently is set to clear
-		if (clearTime > 0) {
-			// decrement clear time
-			--clearTime;
-			// disable rain or thunder
-			thunderTime = isThundering ? 0 : 1;
-			rainTime = isRaining ? 0 : 1;
-			isThundering = false;
-			isRaining = false;
-			// disable blizzard
-			blizzardTime = isBlizzard ? 0 : 1;
-			isBlizzard = false;
-		}
-		// if currently is not clear
-		else {
-			// not thundering, time til next thunder is set already
-			if (thunderTime > 0) {
-				// decrement time til thunder
-				--thunderTime;
-				// starts thunder when time counts to zero
-				if (thunderTime == 0) {
-					isThundering = !isThundering;
-				}
-			} else if (isThundering) {
-				// is thundering, set the time til next thunder
-				thunderTime = this.rand.nextInt(12000) + 3600;
-			} else {
-				// is not thundering now, but time til next thunder is not set, set it
-				thunderTime = this.rand.nextInt(168000) + 12000;
-			}
+        // ignore nether and end etc.
+        if (!this.getDimensionType().hasSkyLight())
+            return;
 
-			// same as thunder logic
-			if (rainTime > 0) {
-				--rainTime;
-				if (rainTime == 0) {
-					isRaining = !isRaining;
-				}
-			} else if (isRaining) {
-				rainTime = this.rand.nextInt(12000) + 12000;
-			} else {
-				rainTime = this.rand.nextInt(168000) + 12000;
-			}
+        // get hourly temp data
+        ClimateData data = ClimateData.get(this);
+        float currentTemp = ClimateData.getTemp(this);
+        System.out.println("Current Temp: " + currentTemp);
 
-			// twr specific blizzard logic
-			if (blizzardTime > 0) {
-				--blizzardTime;
-				if (blizzardTime == 0) {
-					isBlizzard = !isBlizzard;
-				}
-			} else if (isBlizzard) {
-				blizzardTime = this.rand.nextInt(12000) + 12000;
-			} else {
-				blizzardTime = this.rand.nextInt(168000) + 12000;
-			}
-		}
+        // vanilla weather params
+        int clearTime = this.serverWorldInfo.getClearWeatherTime();
+        int rainTime = this.serverWorldInfo.getRainTime();
+        int thunderTime = this.serverWorldInfo.getThunderTime();
+        boolean isRaining = this.serverWorldInfo.isRaining();
+        boolean isThundering = this.serverWorldInfo.isThundering();
 
-		this.serverWorldInfo.setThunderTime(thunderTime);
-		this.serverWorldInfo.setRainTime(rainTime);
-		this.serverWorldInfo.setClearWeatherTime(clearTime);
-		this.serverWorldInfo.setThundering(isThundering);
-		this.serverWorldInfo.setRaining(isRaining);
+        // calculate raining status and blizzard status based on our temp system
+        // 'thundering' is replaced by our BlizzardRenderer
+        isRaining = currentTemp < -4;
+        isThundering = currentTemp < -10;
 
-		// server side change
-		cap.setBlizzard(isBlizzard);
-		cap.setBlizzardTime(blizzardTime);
+        // To make vanilla weather commands work, we still implement the following
+        // This overrides the previous calculation on isRaining and isThundering
+        if (clearTime > 0) {
+            --clearTime;
+            isRaining = false;
+            isThundering = false;
+        }
+        if (rainTime > 0) {
+            --rainTime;
+            isRaining = true;
+            isThundering = false;
+        }
+        if (thunderTime > 0) {
+            --thunderTime;
+            isRaining = true;
+            isThundering = true;
+        }
 
-		// check if need to sync to client
-		if (cap.isBlizzard() != isBlizzard || cap.getBlizzardTime() != blizzardTime) {
-			PacketHandler.send(PacketDistributor.DIMENSION.with(() -> this.getDimensionKey()), new FHClimatePacket(cap));
-		}
-	}
+        this.serverWorldInfo.setThunderTime(thunderTime);
+        this.serverWorldInfo.setRainTime(rainTime);
+        this.serverWorldInfo.setClearWeatherTime(clearTime);
+        this.serverWorldInfo.setThundering(isThundering);
+        this.serverWorldInfo.setRaining(isRaining);
+    }
 
 }
