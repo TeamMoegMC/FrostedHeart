9a17e9ca35cb396ab6c02ffcd230f5aeb9f8b814
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2022-08-16 17:32:40 +0800

    fix airship generation failure

--- src/main/java/com/teammoeg/frostedheart/mixin/minecraft/MinecraftServerMixin.java
+++ src/main/java/com/teammoeg/frostedheart/mixin/minecraft/MinecraftServerMixin.java
@@ -1,8 +1,10 @@
 package com.teammoeg.frostedheart.mixin.minecraft;
 
 import com.teammoeg.frostedheart.world.FHFeatures;
+
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.gen.Heightmap.Type;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraft.world.storage.IServerWorldInfo;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,7 +17,8 @@ public class MinecraftServerMixin {
     @Inject
             (at = @At("TAIL"), cancellable = true, method = "func_240786_a_")
     private static void spacecraftGenerate(ServerWorld serverWorld, IServerWorldInfo info, boolean hasBonusChest, boolean p_240786_3_, boolean p_240786_4_, CallbackInfo ci) {
-        FHFeatures.spacecraft_feature.generate(serverWorld, serverWorld.getChunkProvider().getChunkGenerator(), serverWorld.rand,
+        info.setSpawnY(serverWorld.getHeight(Type.WORLD_SURFACE,info.getSpawnX(),info.getSpawnZ()));
+    	FHFeatures.spacecraft_feature.generate(serverWorld, serverWorld.getChunkProvider().getChunkGenerator(), serverWorld.rand,
                 new BlockPos(info.getSpawnX(), info.getSpawnY(), info.getSpawnZ()));
     }
 }
