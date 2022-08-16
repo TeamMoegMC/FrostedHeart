7bcd97d6e04bd0713e4ddba8b899db627ce5734c
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2022-08-16 19:04:37 +0800

    improved spaceship generation

--- src/main/java/com/teammoeg/frostedheart/mixin/minecraft/MinecraftServerMixin.java
+++ src/main/java/com/teammoeg/frostedheart/mixin/minecraft/MinecraftServerMixin.java
@@ -4,6 +4,7 @@ import com.teammoeg.frostedheart.world.FHFeatures;
 
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.DimensionType;
 import net.minecraft.world.gen.Heightmap.Type;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraft.world.storage.IServerWorldInfo;
@@ -17,7 +18,12 @@ public class MinecraftServerMixin {
     @Inject
             (at = @At("TAIL"), cancellable = true, method = "func_240786_a_")
     private static void spacecraftGenerate(ServerWorld serverWorld, IServerWorldInfo info, boolean hasBonusChest, boolean p_240786_3_, boolean p_240786_4_, CallbackInfo ci) {
-        info.setSpawnY(serverWorld.getHeight(Type.WORLD_SURFACE,info.getSpawnX(),info.getSpawnZ()));
+    	int y=256;
+    	for(int x=info.getSpawnX()-1;x<info.getSpawnX()+1;x++)
+    		for(int z=info.getSpawnZ()-1;z<info.getSpawnZ()+1;z++)
+    			y=Math.min(serverWorld.getHeight(Type.MOTION_BLOCKING_NO_LEAVES,x,z),y);
+    	info.setSpawnY(y-1);
+    	serverWorld.setSpawnLocation(new BlockPos(info.getSpawnX(),info.getSpawnY(),info.getSpawnZ()),info.getSpawnAngle());
     	FHFeatures.spacecraft_feature.generate(serverWorld, serverWorld.getChunkProvider().getChunkGenerator(), serverWorld.rand,
                 new BlockPos(info.getSpawnX(), info.getSpawnY(), info.getSpawnZ()));
     }
--- src/main/java/com/teammoeg/frostedheart/world/feature/SpacecraftFeature.java
+++ src/main/java/com/teammoeg/frostedheart/world/feature/SpacecraftFeature.java
@@ -2,6 +2,11 @@ package com.teammoeg.frostedheart.world.feature;
 
 import com.mojang.serialization.Codec;
 import com.teammoeg.frostedheart.FHMain;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.IWaterLoggable;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.Mirror;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Rotation;
@@ -12,7 +17,10 @@ import net.minecraft.world.ISeedReader;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
+import net.minecraft.world.gen.feature.template.BlockIgnoreStructureProcessor;
+import net.minecraft.world.gen.feature.template.IStructureProcessorType;
 import net.minecraft.world.gen.feature.template.PlacementSettings;
+import net.minecraft.world.gen.feature.template.StructureProcessor;
 import net.minecraft.world.gen.feature.template.Template;
 
 import java.util.Random;
@@ -35,9 +43,13 @@ public class SpacecraftFeature extends Feature<NoFeatureConfig> {
 
         //if (reader.isAirBlock(blockpos$mutable) || reader.getBlockState(blockpos$mutable).getCollisionShapeUncached(reader, blockpos$mutable).isEmpty()) {
         Rotation rot = Rotation.randomRotation(rand);
-        PlacementSettings settings = (new PlacementSettings()).setCenterOffset(new BlockPos(/*9*/9, 2, 7/*8*/)).setRotation(rot).setMirror(Mirror.NONE);
+  
+        PlacementSettings settings = (new PlacementSettings()).setCenterOffset(new BlockPos(/*9*/9, 2, 7/*8*/)).setRotation(rot).setMirror(Mirror.NONE).addProcessor(BlockIgnoreStructureProcessor.STRUCTURE_BLOCK);
+        settings.field_204765_h=false;
         Template template = reader.getWorld().getStructureTemplateManager().getTemplate(new ResourceLocation(FHMain.MODID, "relic/spacecraft"));
         MutableBoundingBox boundingBox = template.getMutableBoundingBox(settings, start);
+
+        
         Vector3i vector3i = boundingBox.func_215126_f();
 
         if (template.func_237146_a_(reader, start, new BlockPos(vector3i.getX(), vector3i.getY(), vector3i.getZ()), settings, reader.getRandom(), 2)) {
