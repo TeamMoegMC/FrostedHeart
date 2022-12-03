957c5b08f231fbab7cc774a34afd8b52280f3eba
dashuaibia <936336596@qq.com>
2022-12-03 20:03:46 +0800

    remove alloy init nbt

---
+++ src/main/java/com/teammoeg/frostedheart/mixin/rankine/MixinAlloyItem.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2022 TeamMoeg
+ *
+ * This file is part of Frosted Heart.
+ *
+ * Frosted Heart is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Frosted Heart is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Frosted Heart. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.teammoeg.frostedheart.mixin.rankine;
+
+import com.cannolicatfish.rankine.items.alloys.AlloyItem;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(AlloyItem.class)
+public class MixinAlloyItem {
+    @Inject(at = @At("HEAD"), method = "inventoryTick", cancellable = true)
+    private void inventoryNBT(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected, CallbackInfo ci) {
+        ci.cancel();
+    }
+}
