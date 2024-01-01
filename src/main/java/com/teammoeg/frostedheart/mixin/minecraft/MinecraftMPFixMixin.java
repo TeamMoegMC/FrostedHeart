fbdc9cc24cee84164d7c47a54d9578c84ddf6d71
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2024-01-01 23:58:17 +0800

    fix missing mixin anno

--- src/main/java/com/teammoeg/frostedheart/mixin/minecraft/MinecraftMPFixMixin.java
+++ src/main/java/com/teammoeg/frostedheart/mixin/minecraft/MinecraftMPFixMixin.java
@@ -1,9 +1,13 @@
 package com.teammoeg.frostedheart.mixin.minecraft;
 
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.minecraft.client.Minecraft;
+
+@Mixin(Minecraft.class)
 public class MinecraftMPFixMixin {
 	@Inject(at = @At("HEAD"), method = "allowsMultiplayer", cancellable = true)
 	public void allowMP(CallbackInfoReturnable<Boolean> callbackInfo) {
