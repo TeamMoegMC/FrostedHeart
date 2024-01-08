28022bc067d5e3e0828b7389b5b5a43aede369a6
khjxiaogu <54445543+khjxiaogu@users.noreply.github.com>
2024-01-08 17:44:31 +0800

    task queue base 1/3

---
+++ src/main/java/com/teammoeg/frostedheart/scheduler/ScheduledData.java
@@ -0,0 +1,8 @@
+package com.teammoeg.frostedheart.scheduler;
+
+import net.minecraft.util.math.BlockPos;
+
+public class ScheduledData {
+	IScheduledTask task;
+	BlockPos pos;
+}
---
+++ src/main/java/com/teammoeg/frostedheart/scheduler/SchedulerQueue.java
@@ -0,0 +1,7 @@
+package com.teammoeg.frostedheart.scheduler;
+
+import java.util.List;
+
+public class SchedulerQueue {
+	List<ScheduledData> tasks;
+}
