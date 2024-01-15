ceafaa5f7268d8005a5b1a61faa7411b0831de0f
khjxiaogu <54445543+khjxiaogu@users.noreply.github.com>
2024-01-15 16:26:41 +0800

    format

--- src/main/java/com/teammoeg/frostedheart/scenario/parser/providers/FTBQProvider.java
+++ src/main/java/com/teammoeg/frostedheart/scenario/parser/providers/FTBQProvider.java
@@ -40,10 +40,8 @@ public class FTBQProvider implements Function<String, Scenario> {
 				b.append("[link l=tsk").append(it).append("]").append(tsk.title==null?"Click here":tsk.title).append("[endlink]")
 				.append("[wa]\n");
 			}else {
-				b.append("@WaitTaskCompleteShow l=tsk").append(it)
-				.append(" q=").append(quest.getCodeString())
-				.append(" t=").append(it)
-				.append("\n").append("@wt\n");
+				b.append("@WaitTaskCompleteShow l=tsk").append(it).append(" q=").append(quest.getCodeString())
+				.append(" t=").append(it).append("\n").append("@wt\n");
 			}
 			b.append("@label name=tsk").append(it).append("\n");
 			if(tsk instanceof CheckmarkTask)
