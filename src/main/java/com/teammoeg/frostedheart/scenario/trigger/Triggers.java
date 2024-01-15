dc83d0b0b2d02c3342c80b7b12e8e575bdd4f3f0
khjxiaogu <54445543+khjxiaogu@users.noreply.github.com>
2024-01-15 14:32:50 +0800

    removed unused

--- src/main/java/com/teammoeg/frostedheart/scenario/trigger/Triggers.java
+++
@@ -1,8 +0,0 @@
-package com.teammoeg.frostedheart.scenario.trigger;
-
-import com.teammoeg.frostedheart.research.NBTSerializerRegistry;
-import com.teammoeg.frostedheart.scenario.runner.TriggerTarget;
-
-public class Triggers {
-	static NBTSerializerRegistry<TriggerTarget> triggers=new NBTSerializerRegistry<TriggerTarget>();
-}
