c159ee55d253af8831d15cc3029c48fc1e9f4c5f
yuesha-yc <kevinwangyichen@gmail.com>
2024-02-24 02:52:07 -0800

    Add null check in setscenario

--- src/main/java/com/teammoeg/frostedheart/scenario/runner/ScenarioVM.java
+++ src/main/java/com/teammoeg/frostedheart/scenario/runner/ScenarioVM.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2024 TeamMoeg
+ *
+ * This file is part of Frosted Heart.
+ *
+ * Frosted Heart is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Frosted Heart is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Frosted Heart. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.teammoeg.frostedheart.scenario.runner;
 
 import java.util.ArrayList;
@@ -54,6 +73,10 @@ public class ScenarioVM implements IScenarioThread{
 	}
 	@Override
 	public void setScenario(Scenario sp) {
+		// Check if the scenario is valid
+		if (sp == null) {
+			throw new ScenarioExecutionException("Scenario is null");
+		}
 		this.sp = sp;
 	}
 	@Override
