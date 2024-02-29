5c4ee9bb5a15ea17bde51e03a62d05ea2c642d15
yuesha-yc <kevinwangyichen@gmail.com>
2024-02-29 15:47:17 -0800

    Add javadocs and rename some town code II

--- src/main/java/com/teammoeg/frostedheart/town/Town.java
+++ src/main/java/com/teammoeg/frostedheart/town/Town.java
@@ -28,10 +28,10 @@ public interface Town {
     /**
      * Adds resource.
      *
-     * @param name     the resouce type
+     * @param name     the resource type
      * @param val      procuded
      * @param simulate simulate process, not actually add.
-     * @return the value that has been add
+     * @return the value that has been added
      */
     double add(TownResourceType name, double val, boolean simulate);
 
@@ -39,16 +39,16 @@ public interface Town {
      * Adds a service,
      * Service is kind or resource that only valid in this tick, and removed when tick ends. It would still count as a resource for get or cost
      *
-     * @param name the resouce type
+     * @param name the resource type
      * @param val  procuded
-     * @return the value that has been add
+     * @return the value that has been added
      */
     double addService(TownResourceType name, double val);
 
     /**
      * Cost a resource
      *
-     * @param name     the resouce type
+     * @param name     the resource type
      * @param val      to be cost
      * @param simulate simulate process, not actually cost.
      * @return the value that has been actually cost.
@@ -58,7 +58,7 @@ public interface Town {
     /**
      * Cost a resource as a service, that means does not actually cost the resource, but would cost temporary at this tick.
      *
-     * @param name     the resouce type
+     * @param name     the resource type
      * @param val      to be cost
      * @param simulate simulate process, not actually cost.
      * @return the value that has been actually cost.
@@ -68,7 +68,7 @@ public interface Town {
     /**
      * Cost a service
      *
-     * @param name     the resouce type
+     * @param name     the resource type
      * @param val      to be cost
      * @param simulate simulate process, not actually cost.
      * @return the value that has been actually cost.
@@ -78,7 +78,7 @@ public interface Town {
     /**
      * Gets resource.
      *
-     * @param name the resouce type
+     * @param name the resource type
      * @return resource amount
      */
     double get(TownResourceType name);
