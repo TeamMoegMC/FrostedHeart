1b5de7c6b9c17acdf80a2134b931658dc9eb824e
yuesha-yc <kevinwangyichen@gmail.com>
2024-07-19 19:28:04 -0400

    Add some package info

---
+++ src/main/java/com/teammoeg/frostedheart/content/package-info.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2024 TeamMoeg
+ *
+ * This file is part of Frosted Heart.
+ *
+ * Frosted Heart is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Frosted Heart is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Frosted Heart. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * This packages includes the systems and game mechanics that must be involved by the player.
+ * This is contrary to the {@link com.teammoeg.frostedheart.world} package, which is everything
+ * that happens before the player joins.
+ * The division is not by function or type of content, like items or blocks, but by the topic
+ * of the content, or mechanics.
+ *
+ * {@link com.teammoeg.frostedheart.content} contains the following subpackages:
+ *
+ * <ul>
+ * <li>{@link com.teammoeg.frostedheart.content.climate}: Climate of the world.  </li>
+ * <li>{@link com.teammoeg.frostedheart.content.agriculture}: Agriculture contents.  </li>
+ * <li>{@link com.teammoeg.frostedheart.content.decoration}: General decorations.   </li>
+ * <li>{@link com.teammoeg.frostedheart.content.foods}: About foods and nutrition system. </li>
+ * <li>{@link com.teammoeg.frostedheart.content.incubator}: About incubation system. </li>
+ * <li>{@link com.teammoeg.frostedheart.content.research}: Research system. </li>
+ * <li>{@link com.teammoeg.frostedheart.content.robotics}: Robotics system for logistics. </li>
+ * <li>{@link com.teammoeg.frostedheart.content.scenario}: Scenario system for lore and tutorial. </li>
+ * <li>{@link com.teammoeg.frostedheart.content.steamenergy}: Steam energy system machines and contents. </li>
+ * <li>{@link com.teammoeg.frostedheart.content.tips}: Tips system for guidance. </li>
+ * <li>{@link com.teammoeg.frostedheart.content.town}: Town system. </li>
+ * <li>{@link com.teammoeg.frostedheart.content.trade}: Trade system with NPCs in other towns. </li>
+ * <li>{@link com.teammoeg.frostedheart.content.trade}: Utility contents not organizable in above pacakges. </li>
+ */
+
+package com.teammoeg.frostedheart.content;
---
+++ src/main/java/com/teammoeg/frostedheart/world/package-info.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2024 TeamMoeg
+ *
+ * This file is part of Frosted Heart.
+ *
+ * Frosted Heart is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Frosted Heart is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Frosted Heart. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * <p>This package includes everything about the world *before* the player joins.
+ * It contains the following subpackages:</p>
+ *
+ * <ul>
+ *   <li>{@link com.teammoeg.world.biome}: Biomes are top level world generation regions.</li>
+ *   <li>{@link com.teammoeg.world.feature}: Features to put in biomes.</li>
+ *   <li>{@link com.teammoeg.world.structure}: Structures to put in biomes.</li>
+ *   <li>{@link com.teammoeg.world.fauna}: The animals. </li>
+ *   <li>{@link com.teammoeg.world.flora}: The plants. </li>
+ * </ul>
+ *
+ */
+
+package com.teammoeg.frostedheart.world;
