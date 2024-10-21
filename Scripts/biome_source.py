2d625656c0be5101988d44cf4e93b9b99c4337c3
yuesha-yc <kevinwangyichen@gmail.com>
2024-10-21 15:39:33 -0700

    Remove all non-winter biomes 2

--- Scripts/biome_source.py
+++ Scripts/biome_source.py
@@ -14,11 +14,27 @@ with open("/Users/wyc/Development/FrostedHeart-World/src/main/resources/data/fro
 biomes = data["dimensions"]["minecraft:overworld"]["generator"]["biome_source"]["biomes"]
 print(len(biomes))
 
-winter_biome_ids = [
+non_snowy_biome_ids = [
+    "minecraft:stony_shore",
+    "minecraft:river",
     "minecraft:ocean",
     "minecraft:deep_ocean",
     "minecraft:cold_ocean",
     "minecraft:deep_cold_ocean",
+    "minecraft:old_growth_pine_taiga",
+    "minecraft:old_growth_spruce_taiga",
+    "terralith:alpine_highlands",
+    "terralith:basalt_cliffs",
+    "terralith:birch_taiga",
+    "terralith:forested_highlands",
+    "terralith:granite_cliffs",
+    "terralith:gravel_desert",
+    "terralith:white_cliffs",
+    "terralith:valley_clearing",
+    "terralith:siberian_taiga",
+]
+
+winter_biome_ids = [
     "minecraft:frozen_ocean",
     "minecraft:deep_frozen_ocean",
     "minecraft:jagged_peaks",
@@ -26,20 +42,15 @@ winter_biome_ids = [
     "minecraft:grove",
     "minecraft:snowy_slopes",
     "minecraft:snowy_taiga",
-    "minecraft:old_growth_pine_taiga",
-    "minecraft:old_growth_spruce_taiga",
-    "minecraft:river",
     "minecraft:frozen_river",
     "minecraft:snowy_beach",
-    "minecraft:stony_shore",
     "minecraft:snowy_plains",
     "minecraft:ice_spikes",
     "minecraft:dripstone_caves",
     "minecraft:lush_caves",
     "terralith:alpine_grove",
-    "terralith:alpine_highlands",
     "terralith:caldera",
-    "terralith:cold_shrubland",
+    "terralith:cold_shrubland", # not enough snow
     "terralith:frozen_cliffs",
     "terralith:glacial_chasm",
     "terralith:ice_marsh",
@@ -47,22 +58,14 @@ winter_biome_ids = [
     "terralith:rocky_shrubland",
     "terralith:scarlet_mountains",
     "terralith:siberian_grove",
-    "terralith:siberian_taiga",
     "terralith:snowy_badlands",
     "terralith:snowy_maple_forest",
     "terralith:snowy_shield",
-    "terralith:valley_clearing",
     "terralith:volcanic_crater",
     "terralith:volcanic_peaks",
     "terralith:wintry_forest",
     "terralith:wintry_lowlands",
     "terralith:yellowstone",
-    "terralith:basalt_cliffs",
-    "terralith:birch_taiga",
-    "terralith:forested_highlands",
-    "terralith:granite_cliffs",
-    "terralith:gravel_desert",
-    "terralith:white_cliffs",
     "terralith:cave/andesite_caves",
     "terralith:cave/desert_caves",
     "terralith:cave/diorite_caves",
@@ -90,12 +93,24 @@ for biome in biomes:
         else:
             repetitions[biome["biome"]] += 1
 
-
 print(len(winter_biomes))
-# sort reptitions
-repetitions = dict(sorted(repetitions.items(), key=lambda item: item[1], reverse=True))
-print(repetitions)
+# repetitions = dict(sorted(repetitions.items(), key=lambda item: item[1], reverse=True))
+# print(repetitions)
+
+# list the biome ids with temperature from high to low
+
+biome_temperatures_high = {}
+biome_temperatures_low = {}
+for biome in winter_biomes:
+    biome_temperatures_high[biome["biome"]] = biome["parameters"]["temperature"][1]
+    biome_temperatures_low[biome["biome"]] = biome["parameters"]["temperature"][0]
+
+biome_temperatures_high = dict(sorted(biome_temperatures_high.items(), key=lambda item: item[1], reverse=True))
+biome_temperatures_low = dict(sorted(biome_temperatures_low.items(), key=lambda item: item[1], reverse=True))
+print(biome_temperatures_high)
+print(biome_temperatures_low)
 
 # save winter biomes as json, with indent=2
+data["dimensions"]["minecraft:overworld"]["generator"]["biome_source"]["biomes"] = winter_biomes
 with open("winter_biomes.json", "w") as f:
-    json.dump(winter_biomes, f, indent=2)
+    json.dump(data, f, indent=2)
