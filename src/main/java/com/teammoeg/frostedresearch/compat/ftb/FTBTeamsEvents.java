405bdaf5ffd9559de002e05139de2b41a4107a58
khjxiaogu <54445543+khjxiaogu@users.noreply.github.com>
2025-03-25 00:28:13 +0800

    fix network

--- src/main/java/com/teammoeg/frostedresearch/compat/ftb/FTBTeamsEvents.java
+++ src/main/java/com/teammoeg/frostedresearch/compat/ftb/FTBTeamsEvents.java
@@ -22,7 +22,7 @@ package com.teammoeg.frostedresearch.compat.ftb;
 import java.util.UUID;
 
 import com.teammoeg.chorda.dataholders.team.CTeamDataManager;
-import com.teammoeg.frostedheart.FHNetwork;
+import com.teammoeg.frostedresearch.FRNetwork;
 import com.teammoeg.frostedresearch.api.ResearchDataAPI;
 import com.teammoeg.frostedresearch.network.FHResearchDataSyncPacket;
 
@@ -45,7 +45,7 @@ public class FTBTeamsEvents {
 
     public static void syncDataWhenTeamChange(PlayerChangedTeamEvent event) {
     	if(event.getPlayer()!=null)
-    		FHNetwork.INSTANCE.sendPlayer(event.getPlayer(),
+    		FRNetwork.INSTANCE.sendPlayer(event.getPlayer(),
                 new FHResearchDataSyncPacket(ResearchDataAPI.getData(event.getPlayer()).get()));
     }
 
@@ -55,7 +55,7 @@ public class FTBTeamsEvents {
 
             CTeamDataManager.INSTANCE.transfer(orig.getId(), event.getTeam());
             for(ServerPlayer p:event.getTeam().getOnlineMembers()) {
-	            FHNetwork.INSTANCE.sendPlayer(p,
+            	FRNetwork.INSTANCE.sendPlayer(p,
 	                    new FHResearchDataSyncPacket(ResearchDataAPI.getData(p).get()));
             }
         }
@@ -69,7 +69,7 @@ public class FTBTeamsEvents {
 
             CTeamDataManager.INSTANCE.transfer(event.getTeam().getId(), orig);
             for(ServerPlayer p:event.getTeam().getOnlineMembers()) {
-	            FHNetwork.INSTANCE.sendPlayer(p,
+            	FRNetwork.INSTANCE.sendPlayer(p,
 	                    new FHResearchDataSyncPacket(ResearchDataAPI.getData(p).get()));
             }
             
