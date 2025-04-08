113e5d4d2e98974aa9f58a9e9810916238159c71
khjxiaogu <54445543+khjxiaogu@users.noreply.github.com>
2025-04-09 03:06:07 +0800

    fix glitch

--- src/main/java/com/teammoeg/frostedresearch/gui/InsightOverlay.java
+++ src/main/java/com/teammoeg/frostedresearch/gui/InsightOverlay.java
@@ -43,6 +43,7 @@ public class InsightOverlay implements IGuiOverlay{
 		//partialTick=PartialTickTracker.getTickAlignedPartialTicks();
 		//FGuis.drawRing(guiGraphics, screenWidth/2, 25, 11, 14, -60,30, 0xff8cffd6);
 		float angleProgress=0;
+		//render animated progress bar if needed
 		if(renderingInsightNextProgress!=renderingInsightProgress)
 			angleProgress=Mth.clamp((renderingInsightNextProgress-renderingInsightProgress)*partialTick+renderingInsightProgress, 0, 1);
 		else
@@ -77,21 +78,21 @@ public class InsightOverlay implements IGuiOverlay{
 		hiddenTicks=0;
 	}
 	public void tick() {
-		if(ClientUtils.mc().isPaused())return;
-		if(textScrollTicks>0) {
+		//if(ClientUtils.mc().isPaused())return;
+		if(textScrollTicks>0) {//text is scrolling, wait until text scroll finished
 			textScrollTicks--;
-			renderingInsightLevel=renderingNextInsightLevel;
-			if(textScrollTicks<=0) {
+			renderingInsightProgress=renderingInsightNextProgress;
+			if(textScrollTicks<=0) {//text scroll finish, maintain progress bar to next/prev level
 				textScrollTicks=0;
+				
 				if(renderingInsightLevel>renderingNextInsightLevel) {
-					renderingInsightLevel=renderingNextInsightLevel;
 					renderingInsightNextProgress=1;
 				}else if(renderingInsightLevel<renderingNextInsightLevel) {
-					renderingInsightLevel=renderingNextInsightLevel;
 					renderingInsightNextProgress=0;
 				}
-			}
-			return;
+				renderingInsightLevel=renderingNextInsightLevel;
+			}else
+				return;
 		}
 		TeamResearchData rd=ClientResearchDataAPI.getData().get();
 		int curInsightLevel=rd.getInsightLevel();
@@ -99,13 +100,19 @@ public class InsightOverlay implements IGuiOverlay{
 		renderingInsightProgress=renderingInsightNextProgress;
 		float delta=(curInsightLevel-renderingInsightLevel)+curInsightProgress-renderingInsightProgress;
 		if(delta!=0) {
+			//display overlay when animation needed
 			isShown=true;
 			hiddenTicks=LINGERING_TICKS;
 			float direction=Math.signum(delta);
 			float value=Math.abs(delta);
-			float dividedvalue=Mth.clamp(value/5F, MIN_PROGRESS_PRE_TICK, MAX_PROGRESS_PRE_TICK);
-			value=Math.min(value, dividedvalue);
+			//calculate smoothed value
+			float smoothedvalue=Mth.clamp(value/5F, MIN_PROGRESS_PRE_TICK, MAX_PROGRESS_PRE_TICK);
+			
+			//do not obey min-progress if smoothed value is larger than value itself
+			value=Math.min(value, smoothedvalue);
+			//calculate animated progress for next tick, and smooth the animation with partialticks
 			renderingInsightNextProgress+=value*direction;
+			//progress over 1 or lower than 0, the level should be modified
 			if(renderingInsightNextProgress<0) {
 				renderingInsightNextProgress=0;
 				renderingNextInsightLevel=renderingInsightLevel-1;
@@ -115,7 +122,7 @@ public class InsightOverlay implements IGuiOverlay{
 				renderingNextInsightLevel=renderingInsightLevel+1;
 				textScrollTicks=LEVEL_SCROLL_TICKS;
 			}
-		}else if(isShown) {
+		}else if(isShown) {//all animation has done, wait for several ticks before hidding this overlay
 			hiddenTicks--;
 			if(hiddenTicks<=0) {
 				isShown=false;
