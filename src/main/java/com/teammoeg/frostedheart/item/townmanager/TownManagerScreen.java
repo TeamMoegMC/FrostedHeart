c9f53cafea1891c05ede3a7069963f9b8a4a085f
duckegg128 <changxietian@163.com>
2025-12-31 23:40:52 +0800

    TownManagerScreen增加居民列表及信息

--- src/main/java/com/teammoeg/frostedheart/item/townmanager/TownManagerScreen.java
+++ src/main/java/com/teammoeg/frostedheart/item/townmanager/TownManagerScreen.java
@@ -6,6 +6,7 @@ import com.teammoeg.frostedheart.FHMain;
 import com.teammoeg.frostedheart.bootstrap.common.FHSpecialDataTypes;
 import com.teammoeg.frostedheart.content.town.TeamTown;
 import com.teammoeg.frostedheart.content.town.TownWorkerData;
+import com.teammoeg.frostedheart.content.town.resident.Resident;
 import com.teammoeg.frostedheart.util.client.FHClientUtils;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
@@ -40,10 +41,13 @@ public class TownManagerScreen extends Screen {
         this.topPos = (this.height - this.imageHeight) / 2;
         if(town != null){
             if(this.activeScreenMode == null){
-                setScreenMode(getBlockListScreenMode());
+                setScreenMode(getResidentListScreenMode());
             } else{
                 //这里应该调用所有会存在Screen里的Mode的初始化方法，但是现在只有这一个的init方法需要调用，所以只调用这个。
                 blockListScreenMode.init();
+                blockInfoScreenMode.init();
+                residentInfoScreenMode.init();
+                residentListScreenMode.init();
                 activeScreenMode.init();
 
                 activeScreenMode.onEnter();
@@ -250,8 +254,14 @@ public class TownManagerScreen extends Screen {
     }
 
     private BlockInfoScreenMode getBlockInfoScreenMode(TownWorkerData data){
-        return new BlockInfoScreenMode(data);
+        if(blockInfoScreenMode == null){
+            blockInfoScreenMode = new BlockInfoScreenMode(data);
+        } else{
+            blockInfoScreenMode.workerData = data;
+        }
+        return blockInfoScreenMode;
     }
+    private BlockInfoScreenMode blockInfoScreenMode;
     class BlockInfoScreenMode implements ScreenMode{
 
         //城镇方块的workerData
@@ -281,6 +291,180 @@ public class TownManagerScreen extends Screen {
         @Override
         public int getTabBelongTo() {
             return BLOCK_TAB;
+        }
+
+        @Override
+        public void onEnter() {
+            addRenderableWidget(backButton);
+        }
+
+        @Override
+        public void onExit() {
+            removeWidget(backButton);
+        }
+
+        @Override
+        public void init() {
+            this.backButton.setX(leftPos + 10);
+            this.backButton.setY(topPos + 10);
+        }
+    }
+
+    private ResidentListScreenMode getResidentListScreenMode(){
+        if(residentListScreenMode == null){
+            residentListScreenMode = new ResidentListScreenMode();
+        }
+        return residentListScreenMode;
+    }
+    private ResidentListScreenMode residentListScreenMode;
+    class ResidentListScreenMode implements ScreenMode{
+
+        //由于这个的会随Scrollbar改变，直接以Widget形式加入
+        List<Button> residentButtons;
+
+        ScrollBar scrollBar;
+
+        //当前可见的行数
+        int totalVisibleRows = imageHeight / 20;
+
+        ResidentListScreenMode(){
+            this.scrollBar = new ScrollBar(leftPos + imageWidth - 10, topPos, 10, imageHeight, town.getAllResidents().size() - totalVisibleRows + 1)
+                    .setOnRowChanged(this::onRowChanged)
+                    /*.setThumbTexture(TEXTURE)todo:之后再加*/;
+            this.residentButtons = new ArrayList<>();
+            int i = 0;
+            for(Resident resident : town.getAllResidents()){
+                Button button = new Button.Builder(Component.literal(i+1 + " " + resident.getFirstName() + " " + resident.getLastName()), (pButton -> {
+                    setScreenMode(getResidentInfoScreenMode(resident));
+                }))
+                        .size(imageWidth - 10, 20)
+                        .pos(leftPos, topPos + 20 * i)
+                        .build();
+                residentButtons.add(button);
+                i++;
+                System.out.println("duck_egg debug: adding resident button for: " + resident.getFirstName() + " " + resident.getLastName());
+                System.out.println("duck_egg debug: resident button pos: " + button.getX() + " " + button.getY());
+            }
+        }
+
+        public void onRowChanged(int rowOld, int rowNew) {
+            if (rowOld == rowNew) return;
+
+            int oldEnd = Math.min(rowOld + totalVisibleRows, residentButtons.size());
+            int newEnd = Math.min(rowNew + totalVisibleRows, residentButtons.size());
+
+            // 计算需要移除和添加的范围
+            int removeStart = (rowNew > rowOld) ? rowOld : newEnd;
+            int removeEnd = (rowNew > rowOld) ? Math.min(rowNew, oldEnd) : oldEnd;
+
+            int addStart = (rowNew > rowOld) ? Math.max(oldEnd, rowNew) : rowNew;
+            int addEnd = (rowNew > rowOld) ? newEnd : Math.min(rowOld, newEnd);
+
+            // 移除不再可见的按钮
+            for (int i = removeStart; i < removeEnd; i++) {
+                removeWidget(residentButtons.get(i));
+            }
+
+            // 添加新可见的按钮并更新所有可见按钮的位置
+            for (int i = rowNew; i < newEnd; i++) {
+                int relativeRow = i - rowNew;
+                Button button = residentButtons.get(i);
+                button.setY(topPos + 20 * relativeRow);
+                if (i >= addStart && i < addEnd) {
+                    addRenderableWidget(button);
+                }
+            }
+        }
+
+        @Override
+        public int getTabBelongTo() {
+            return RESIDENT_TAB;
+        }
+
+        @Override
+        public void onEnter() {
+            addRenderableWidget(scrollBar);
+            int currentRow = scrollBar.getCurrentRow();
+            for(int i = currentRow; i < currentRow + totalVisibleRows && i < residentButtons.size() ; i++){
+                addRenderableWidget(residentButtons.get(i));
+            }
+        }
+
+        @Override
+        public void onExit() {
+            removeWidget(scrollBar);
+            int currentRow = scrollBar.getCurrentRow();
+            for(int i = currentRow; i < currentRow + totalVisibleRows && i < residentButtons.size() ; i++){
+                removeWidget(residentButtons.get(i));
+            }
+        }
+
+        @Override
+        public void init() {
+            //刷新可见行数
+            this.totalVisibleRows = imageHeight / 20;
+            //刷新ScrollBar
+            if(scrollBar != null){
+                int currentRow = scrollBar.getCurrentRow();
+                this.scrollBar = new ScrollBar(leftPos + imageWidth - 10, topPos, 10, imageHeight, town.getAllResidents().size() - totalVisibleRows + 1)
+                        .setOnRowChanged(this::onRowChanged)
+                /*.setThumbTexture(TEXTURE)todo:之后再加*/;
+                scrollBar.setCurrentRow(currentRow);
+            } else {
+                this.scrollBar = new ScrollBar(leftPos + imageWidth - 10, topPos, 10, imageHeight, town.getAllResidents().size() - totalVisibleRows + 1)
+                        .setOnRowChanged(this::onRowChanged)
+                /*.setThumbTexture(TEXTURE)todo:之后再加*/;
+            }
+            //刷新所有按钮X坐标
+            for(Button button : residentButtons){
+                button.setX(leftPos);
+            }
+            //刷新可见按钮Y坐标
+            for(int i = scrollBar.getCurrentRow(); i < scrollBar.getCurrentRow() + totalVisibleRows && i < residentButtons.size(); i++){
+                Button button = residentButtons.get(i);
+                button.setY(topPos + 20 * (i - scrollBar.getCurrentRow()));
+            }
+        }
+    }
+
+
+    private ResidentInfoScreenMode getResidentInfoScreenMode(Resident resident){
+        if(residentInfoScreenMode == null){
+            residentInfoScreenMode = new ResidentInfoScreenMode(resident);
+        } else{
+            residentInfoScreenMode.resident = resident;
+        }
+        return residentInfoScreenMode;
+    }
+    private ResidentInfoScreenMode residentInfoScreenMode;
+    class ResidentInfoScreenMode implements ScreenMode{
+        Resident resident;
+
+        Button backButton = new Button.Builder(Component.literal("Back"/*todo: Translation Key*/)//todo: 应为ImageButton，弄个往左指的箭头
+                , (pButton -> setScreenMode(getResidentListScreenMode())))
+                .size(10, 10)
+                .pos(leftPos + 10, topPos + 10)
+                .build();
+
+        ResidentInfoScreenMode(Resident resident){
+            this.resident = resident;
+        }
+
+        @Override
+        public void render(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
+            if(resident == null){
+                pGuiGraphics.drawString(font, Component.translatable("frostedheart.town_manager.no_resident_selected"), leftPos + 10, topPos + 20, 0xFFFFFF);
+                return;
+            }
+            pGuiGraphics.drawString(font, Component.translatable("frostedheart.town_manager.resident_name").append(" : " + resident.getFirstName() + " " + resident.getLastName()), leftPos + 10, topPos + 20, 0xFFFFFF);
+            pGuiGraphics.drawString(font, Component.translatable("frostedheart.town_manager.resident_health").append(" : " + resident.getHealth()), leftPos + 10, topPos + 40, 0xFFFFFF);
+            pGuiGraphics.drawString(font, Component.translatable("frostedheart.town_manager.resident_mental").append(" : " + resident.getMental()), leftPos + 10, topPos + 60, 0xFFFFFF);
+            //todo: more info need to be added, maybe second page needed? I don't know...
+        }
+
+        @Override
+        public int getTabBelongTo() {
+            return RESIDENT_TAB;
         }
 
         @Override
