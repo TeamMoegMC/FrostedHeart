cbded1dc7ab748ca7ff1101ce4b2ace3114287bb
khjxiaogu <54445543+khjxiaogu@users.noreply.github.com>
2026-01-28 17:49:41 +0800

    bot dock ui base

--- src/main/java/com/teammoeg/frostedheart/content/robotics/logistics/gui/LogisticChestScreen.java
+++ src/main/java/com/teammoeg/frostedheart/content/robotics/logistics/gui/LogisticChestScreen.java
@@ -19,10 +19,13 @@
 
 package com.teammoeg.frostedheart.content.robotics.logistics.gui;
 
+import java.util.function.Consumer;
+
 import com.teammoeg.frostedheart.util.client.FHClientUtils;
 import blusunrize.immersiveengineering.client.gui.IEContainerScreen;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 
 public class LogisticChestScreen extends IEContainerScreen<LogisticChestMenu> {
@@ -30,5 +33,36 @@ public class LogisticChestScreen extends IEContainerScreen<LogisticChestMenu> {
 
     public LogisticChestScreen(LogisticChestMenu inventorySlotsIn, Inventory inv, Component title) {
         super(inventorySlotsIn, inv, title,TEXTURE);
+        this.imageHeight = 200;
+        this.inventoryLabelY = this.imageHeight - 94;
     }
+    @Override
+	protected void drawBackgroundTexture(GuiGraphics graphics) {
+		graphics.blit(background, leftPos, topPos, 0, 0, imageWidth, imageHeight);
+	}
+
+	@Override
+	protected void drawContainerBackgroundPre(GuiGraphics matrixStack, float partialTicks, int x, int y) {
+
+	}
+
+	protected void renderLabels(GuiGraphics matrixStack, int x, int y) {
+		// titles
+		matrixStack.drawString(this.font, this.title, this.titleLabelX, this.titleLabelY, 0xff404040);
+		// this.font.drawText(matrixStack, this.playerInventory.getDisplayName(),
+		// this.playerInventoryTitleX, this.playerInventoryTitleY+5, 0xff404040); 
+	}
+
+	@Override
+	public void init() {
+		super.init();
+
+
+	}
+
+	@Override
+	protected void gatherAdditionalTooltips(int mouseX, int mouseY, Consumer<Component> addLine, Consumer<Component> addGray) {
+		super.gatherAdditionalTooltips(mouseX, mouseY, addLine, addGray);
+	
+	}
 }
