c5e7e22fd9726bebe56ddd180a50ec3f8027887d
khjxiaogu <54445543+khjxiaogu@users.noreply.github.com>
2026-02-05 18:09:12 +0800

    more units

--- src/main/java/com/teammoeg/chorda/text/CFormatHelper.java
+++ src/main/java/com/teammoeg/chorda/text/CFormatHelper.java
@@ -1,11 +1,13 @@
 package com.teammoeg.chorda.text;
 
 import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.time.Duration;
 
 import net.minecraft.core.Holder;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.util.Mth;
 import net.minecraft.world.level.biome.Biome;
 
 public class CFormatHelper {
@@ -46,8 +48,8 @@ public class CFormatHelper {
 	    Duration duration = Duration.ofMillis(milliseconds);
 	    return secToTime(duration.getSeconds());
 	}
-	final static String units = "KMGTPEZYH";
-	final static String fractionUnits="mμnpf";
+	final static String units = "kMGTPEZYRQ";//6 units should satisfy
+	final static String fractionUnits="mμnpfazyrq";
 	public static class NumberFormats{
 		private NumberFormats() {}
 		final DecimalFormat decimal1digit = new DecimalFormat("#.#");
@@ -62,6 +64,7 @@ public class CFormatHelper {
 	public static NumberFormats getNumberFormats() {
 		return numberFormat.get();
 	}
+
 	public static String toReadableUnit(long num) {
 		int unit = -1;
 		double lnum = num;
@@ -72,6 +75,72 @@ public class CFormatHelper {
 		if (unit < 0)
 			return String.valueOf(num);
 		return toDynamicDigits(lnum) + units.charAt(unit);
+	}
+	public static String toBigReadableUnit(String bigNumber) {
+		int index=0,exponent=0,unit=0,fractionDigits=3;
+		char[] digits=new char[3];
+		boolean isFraction=false;
+		for(;index<bigNumber.length();index++) {
+			char c=bigNumber.charAt(index);
+			if(c>='0'&&c<='9') {
+				if(digits[0]==0) {//at start
+					if(isFraction)
+						exponent--;
+					if(c=='0')
+						continue;
+					digits[0]=c;
+				}else if(digits[1]==0) {
+					digits[1]=c;
+				}else if(digits[2]==0) {
+					digits[2]=c;
+					if(isFraction)
+						break;
+				}
+				if(!isFraction)
+					exponent++;
+			}else if(c=='.')
+				isFraction=true;
+		}
+		if(exponent>0)
+			unit=(int) Math.floor((exponent-1)/3f);
+		else
+			unit=(int) Math.floor((exponent)/3f);
+		int dot=exponent%3;
+		if(dot<0)
+			dot=(dot+4);
+		
+		for(int i=2;i>=0;i--) {
+			if(digits[i]==0) {
+				digits[i]='0';
+			}
+			if(digits[i]=='0'&&i>=dot) {
+				fractionDigits=i;
+			}
+		}
+		if(fractionDigits==0)
+			return "0";
+		StringBuilder sb=new StringBuilder();
+		if(digits[0]!=0)
+			for(int i=0;i<fractionDigits;i++) {
+				sb.append(digits[i]);
+				if(i==(dot-1)&&i!=fractionDigits-1)
+					sb.append(".");
+			}
+		if(unit<0)
+			sb.append(fractionUnits.charAt(-unit-1));
+		else if(unit>0)
+			sb.append(units.charAt(unit-1));
+		return sb.toString();
+	}
+	public static void main(String[] args) {
+		for(int i=7;i>=0;i--) {
+			String num="9".repeat(i);
+		System.out.println(i+"="+toBigReadableUnit(num));
+		}
+		for(int i=0;i<6;i++) {
+			String num="0."+("0".repeat(i))+"9";
+		System.out.println("-"+i+"="+toBigReadableUnit(num));
+		}
 	}
 	public static String toReadableItemStackUnit(long num) {
 		int unit = -1;
