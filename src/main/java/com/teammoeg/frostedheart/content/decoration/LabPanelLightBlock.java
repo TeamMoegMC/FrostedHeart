dbc90dd6c204bef295d5cfffc495687ad031dfba
khjxiaogu <54445543+khjxiaogu@users.noreply.github.com>
2026-02-08 03:14:51 +0800

    light rotation

--- src/main/java/com/teammoeg/frostedheart/content/decoration/LabPanelLightBlock.java
+++ src/main/java/com/teammoeg/frostedheart/content/decoration/LabPanelLightBlock.java
@@ -19,8 +19,11 @@
 
 package com.teammoeg.frostedheart.content.decoration;
 
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.RedstoneLampBlock;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -34,4 +37,17 @@ public class LabPanelLightBlock extends RedstoneLampBlock {
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
 		super.createBlockStateDefinition(pBuilder.add(BlockStateProperties.HORIZONTAL_FACING));
 	}
+	@Override
+    public BlockState rotate(BlockState pState, Rotation pRot) {
+        return pState.setValue(BlockStateProperties.HORIZONTAL_FACING, pRot.rotate(pState.getValue(BlockStateProperties.HORIZONTAL_FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState pState, Mirror pMirror) {
+        return pState.setValue(BlockStateProperties.HORIZONTAL_FACING, pMirror.mirror(pState.getValue(BlockStateProperties.HORIZONTAL_FACING)));
+    }
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return this.defaultBlockState().setValue(BlockStateProperties.HORIZONTAL_FACING, pContext.getHorizontalDirection().getOpposite());
+    }
 }
