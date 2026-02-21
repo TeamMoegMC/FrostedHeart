bbde5be86a831cb45ef6ffe5379898d45abeefdb
duckegg128 <changxietian@163.com>
2026-02-21 17:43:32 +0800

    修复merge未解决的问题

--- src/main/java/com/teammoeg/frostedheart/bootstrap/common/FHMenuTypes.java
+++ src/main/java/com/teammoeg/frostedheart/bootstrap/common/FHMenuTypes.java
@@ -22,7 +22,6 @@ package com.teammoeg.frostedheart.bootstrap.common;
 import blusunrize.immersiveengineering.api.multiblocks.blocks.logic.IMultiblockState;
 import com.teammoeg.frostedheart.content.town.buildings.warehouse.WarehouseMenu;
 import com.teammoeg.frostedheart.content.town.buildings.warehouse.WarehouseBlockEntity;
-import com.teammoeg.frostedheart.item.snowsack.SnowSackMenu;
 import org.apache.commons.lang3.mutable.Mutable;
 import org.apache.commons.lang3.mutable.MutableObject;
 
